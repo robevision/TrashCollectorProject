a944a2a2aa5e06a0fa1abc390566294d1804fc75
Robe Gogan <robe.gogan@gmail.com>
2019-03-05 09:13:08 -0600

    Merged the local repository with the virtual repository that includes basic framework setup for an ASP.NET MVC application.

---
+++ Trash_Collector_Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Trash_Collector_Project.Startup))]
+namespace Trash_Collector_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
