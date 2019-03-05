42e9a78f182e9bd730df8412ef61cb765c131c4b
Robe Gogan <robe.gogan@gmail.com>
2019-03-05 15:29:14 -0600

    Under Startup class, an additional method was added to the Configuration method and the logic for method to allow for roles to be administered on startup of application.

--- Trash_Collector_Project/Startup.cs
+++ Trash_Collector_Project/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using Trash_Collector_Project.Models;
 
 [assembly: OwinStartupAttribute(typeof(Trash_Collector_Project.Startup))]
 namespace Trash_Collector_Project
@@ -9,6 +12,47 @@ namespace Trash_Collector_Project
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
         }
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                var user = new ApplicationUser();
+                user.UserName = "EricFreeman";
+                user.Email = "garbageday@ericfreeman.com";
+
+                string userPWD = "poiuyt";
+
+                var chkUser = userManager.Create(user, userPWD);
+
+                if (chkUser.Succeeded)
+                {
+                    var result1 = userManager.AddToRole(user.Id, "Admin");
+                }
+            }
+
+        if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
+        if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+            }
+        }
+       
     }
 }
