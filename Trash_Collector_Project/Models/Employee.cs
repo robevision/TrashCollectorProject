7d17dc947c5efad9aee07fed726f11a510027e99
Robe Gogan <robe.gogan@gmail.com>
2019-03-06 09:57:02 -0600

    Built the Employee Model class and added property attributes.

---
+++ Trash_Collector_Project/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trash_Collector_Project.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        [Display(Name = "Zip Code")]
+        public int ZipCode { get; set; }
+    }
+}
